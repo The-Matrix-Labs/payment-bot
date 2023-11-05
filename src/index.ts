41e2718adcb01d93fa66e5daee524ae91670b331
Vishwa Vivek Yadav <112635764+Vishwa9011@users.noreply.github.com>
2023-11-05 16:39:19 +0530

    testing bot

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 // Define the bot's webhook endpoint
 // app.use(bot.webhookCallback("/bot"));
 
